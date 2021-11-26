b7c66ed67a299a0e0a0954b003d0e7073bf1b88c
Farrukh Shaikh <52906883+FarrukhShaikh21@users.noreply.github.com>
2021-11-27 02:01:34 +0500

    adding file upload via mobile and SOAP service

--- UI/src/pqt/gh/elaaj/view/request/BenefitDetailBean.java
+++ UI/src/pqt/gh/elaaj/view/request/BenefitDetailBean.java
@@ -1,21 +1,43 @@
 package pqt.gh.elaaj.view.request;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.io.OutputStream;
+
 import javax.el.ELContext;
 
 import javax.el.MethodExpression;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+
+import javax.faces.event.ValueChangeEvent;
 
 import oracle.adf.model.BindingContext;
 
+import oracle.adf.model.binding.DCBindingContainer;
+import oracle.adf.model.binding.DCIteratorBinding;
 import oracle.adf.view.rich.component.rich.data.RichListView;
+
+import oracle.adf.view.rich.component.rich.input.RichInputFile;
 
 import oracle.binding.BindingContainer;
 import oracle.binding.OperationBinding;
 
+import oracle.jbo.Row;
+
+import oracle.jbo.ViewObject;
+
+import org.apache.myfaces.trinidad.model.UploadedFile;
+
 public class BenefitDetailBean {
     private RichListView selectMaster;
-
+    private RichInputFile rif;
+    private String FileDownloadPath = "/u01/ADF_DMS/BTCS_CMS_FILE/DOWNLOAD/";
+//    private String FileUploadPath = "/u01/ADF_DMS/BTCS_CMS_FILE/UPLOAD/";
+    private String FileUploadPath = "C:\\hello\\";
     public BenefitDetailBean() {
     }
     
@@ -42,5 +64,130 @@ public class BenefitDetailBean {
 
     public RichListView getSelectMaster() {
         return selectMaster;
+    }
+    public void uploadFileEventClaimReimburse(ValueChangeEvent vc) {
+        if (vc.getNewValue() != null) {
+
+            UploadedFile fileVal = (UploadedFile) vc.getNewValue();
+            BindingContext bcx = BindingContext.getCurrent();
+            BindingContainer bc = bcx.getCurrentBindingsEntry();
+            DCBindingContainer dbc = (DCBindingContainer) bc;
+            DCIteratorBinding iter = dbc.findIteratorBinding("ElaajClaimReimburstmentEOVO1Iterator");
+
+            String vLineIDPk = null;
+            //            vtime = (String)System.currentTimeMillis();
+            Row row = iter.getCurrentRow();
+            try {
+                vLineIDPk = row.getAttribute("ElaajClaimReimburstmentId").toString();
+            } catch (NullPointerException ex) {
+                vLineIDPk = null;
+            }
+            //            String lineIdPk = null;
+            String filePath = uploadFile(fileVal,"ElaajClaimReimburstmentEOVO1Iterator",vLineIDPk);
+    //            row.setAttribute("FilePath", filePath);
+            //            row.setAttribute("LineIdPk", lineIdPk);
+            
+
+            String fileName = fileVal.getFilename();
+            row.setAttribute("AttachFile1", vLineIDPk + fileName);
+
+            String fileType = fileVal.getContentType();
+            fileUploadAction(vLineIDPk + fileName);
+            getRif().resetValue();
+    //            row.setAttribute("ContentType", fileType);
+        }
+    } 
+     
+    public void uploadFileEventClaimIntimation(ValueChangeEvent vc) {
+        if (vc.getNewValue() != null) {
+
+            UploadedFile fileVal = (UploadedFile) vc.getNewValue();
+            BindingContext bcx = BindingContext.getCurrent();
+            BindingContainer bc = bcx.getCurrentBindingsEntry();
+            DCBindingContainer dbc = (DCBindingContainer) bc;
+            DCIteratorBinding iter = dbc.findIteratorBinding("ElaajClaimIntimateEOVO1Iterator");
+
+            String vLineIDPk = null;
+            //            vtime = (String)System.currentTimeMillis();
+            Row row = iter.getCurrentRow();
+            try {
+                vLineIDPk = row.getAttribute("ElaajClaimIntimateId").toString();
+            } catch (NullPointerException ex) {
+                vLineIDPk = null;
+            }
+
+            String filePath = uploadFile(fileVal,"ElaajClaimIntimateEOVO1Iterator",vLineIDPk);
+//            row.setAttribute("FilePath", filePath);
+            //            row.setAttribute("LineIdPk", lineIdPk);
+           
+            String fileName = fileVal.getFilename();
+            row.setAttribute("AttachFile1", vLineIDPk + fileName);
+
+            String fileType = fileVal.getContentType();
+            fileUploadAction(vLineIDPk + fileName);
+            getRif().resetValue();
+//            row.setAttribute("ContentType", fileType);
+        }
+    } 
+ 
+    private String uploadFile(UploadedFile file,String pIteratorBinding, String pClaimId) {
+        
+            //        System.out.println("FileUploadPath: " + FileUploadPath);
+
+//            BindingContext bcx = BindingContext.getCurrent();
+//            BindingContainer bc = bcx.getCurrentBindingsEntry();
+//            DCBindingContainer dbc = (DCBindingContainer) bc;
+//            DCIteratorBinding iter = dbc.findIteratorBinding(pIteratorBinding);
+
+            String vLineIDPk = pClaimId;
+            //        row.setAttribute("LineIdPk", lineIdPk);
+
+            UploadedFile myfile = file;
+            System.out.println("this file path");
+            System.out.println(FileUploadPath + vLineIDPk + myfile.getFilename());
+            if (myfile != null) {
+                try {
+                    InputStream is = file.getInputStream();
+                    
+                    OutputStream os = new FileOutputStream(FileUploadPath + vLineIDPk + myfile.getFilename());
+                    int readData;
+                    while ((readData = is.read()) != -1) {
+                        os.write(readData);
+                    }
+                    is.close();
+                    os.close();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                    return "Error while Uploading";
+                }
+            }
+            return FileUploadPath + vLineIDPk + myfile.getFilename();
+        }   
+ 
+    public String fileUploadAction(String pFileName) {
+    
+    BindingContainer bc = getBindings();
+    OperationBinding ob = bc.getOperationBinding("UploadFile1");
+
+   
+    ob.getParamsMap().put("DocumentReference", "Create");
+    ob.getParamsMap().put("SchemaName", "FT_Elaaj_Claims");
+    ob.getParamsMap().put("RepoName", "FT_Elaaj_Claims");
+    ob.getParamsMap().put("PrimaryKey", "UL_NO," + "vUL_No");
+    ob.getParamsMap().put("FileLocation", FileUploadPath);
+    ob.getParamsMap().put("FileName", pFileName);
+    ob.getParamsMap().put("Parameters", "UL_NO," + "vUL_No");
+    Object result = ob.execute();
+    
+    return null;
+    }
+
+
+    public void setRif(RichInputFile rif) {
+        this.rif = rif;
+    }
+
+    public RichInputFile getRif() {
+        return rif;
     }
 }
